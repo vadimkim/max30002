75ba957c38aa8857f66b281c704223b57ae74c39
vadim <vadim@ant.ee>
2024-03-21 22:36:38 +0200

    class, constructor and info register definitions

--- max30002.cpp
+++ max30002.cpp
@@ -1,8 +1,12 @@
 /*
-* Created on: 21.03.24
-* Author: vadimkim
-*
-*/
+ * Created on: 21.03.24
+ * Author: vadimkim
+ *
+ */
 
-#include "mbed.h"
 #include "max30002.h"
+#include "mbed.h"
+
+MAX30002::MAX30002(SPI &spi, DigitalOut &cs) : m_spi(spi), m_cs(cs) {
+  m_cs = 1;
+}
--- max30002.h
+++ max30002.h
@@ -1,7 +1,51 @@
 /*
-* Created on: 21.03.24
-* Author: vadimkim
-*
-*/
+ * Created on: 21.03.24
+ * Author: vadimkim
+ *
+ */
 
 #include "mbed.h"
+#include <cstdint>
+
+class MAX30002 {
+public:
+  enum class REG_map_t : uint8_t { // register map
+    STATUS = 0x01,
+    EN_INT = 0x02,
+    EN_INT2 = 0x03,
+    MNGR_INT = 0x04,
+    MNGR_DYN = 0x05,
+    SW_RST = 0x08,
+    SYNCH = 0x09,
+    FIFO_RST = 0x0A,
+    INFO = 0x0F,
+    CNFG_GEN = 0x10,
+    CNFG_BMUX = 0x17,
+    CNFG_BIOZ = 0x18,
+    FIFO_BURST = 0x22,
+    FIFO = 0x23
+  };
+
+  /*
+   * @brief INFO (0x0F)
+   */
+  union max30001_info_reg {
+    uint8_t all;
+    struct {
+      uint8_t reserved : 4;
+      uint8_t rev_id : 4;
+    } bit;
+  };
+
+  /*
+   * @brief MAX30002 constructor
+   * @param spi SPI object
+   * @param cs PIN for chip select
+   */
+  MAX30002(SPI &spi, DigitalOut &cs);
+
+private:
+  SPI &m_spi;
+  DigitalOut &m_cs;
+
+}; // end of class
